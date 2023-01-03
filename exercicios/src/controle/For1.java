69928ed16b2f6b61c041580c4fd5ffd8c0329fcd
joseluizs <joseluiz.smarques@gmail.com>
2023-01-03 19:50:14 -0300

    For1
    For determinado

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		/*
+		 * int count = 1;
+		 * 
+		 * while (count <= 10) { System.out.printf(" i = %d\n", count); count++; }
+		 */
+		for (int count = 1; count <= 10 ; count++) {
+			System.out.printf(" i = %d\n", count);
+		}
+
+	}
+
+}
