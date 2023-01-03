ff49a4b1e9ca7c029bbe6c0b91383e38394bdd58
joseluizs <joseluiz.smarques@gmail.com>
2023-01-03 19:44:31 -0300

    WhileDeterminado

--- exercicios/src/controle/DiaDaSemana.java
+++ exercicios/src/controle/DiaDaSemana.java
@@ -12,7 +12,7 @@ public class DiaDaSemana {
 		}
 		else if (dia.equalsIgnoreCase("segunda")) {
 			JOptionPane.showInternalMessageDialog(null, "2");			
-		} else if (dia.equalsIgnoreCase("terça")) {
+		} else if (dia.equalsIgnoreCase("terca") || dia.equalsIgnoreCase("terça")) {
 			JOptionPane.showInternalMessageDialog(null, "3");			
 		} else if(dia.equalsIgnoreCase("quarta")) {
 			JOptionPane.showInternalMessageDialog(null, "4");		
@@ -20,8 +20,10 @@ public class DiaDaSemana {
 			JOptionPane.showInternalMessageDialog(null, "5");			
 		} else if (dia.equalsIgnoreCase("sexta")) {
 			JOptionPane.showInternalMessageDialog(null, "6");			
-		} else {
+		} else if (dia.equalsIgnoreCase("sabado") || dia.equalsIgnoreCase("sábado")) {
 			JOptionPane.showInternalMessageDialog(null, "7");
+		} else {
+			JOptionPane.showInternalMessageDialog(null, "Dia inválido!");
 		}
 		JOptionPane.showInternalMessageDialog(null, "Fim");
 	}
