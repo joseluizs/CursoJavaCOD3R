6e15f6182d188df2c067ad83b79d10116807b0f6
joseluizs <joseluiz.smarques@gmail.com>
2023-01-06 12:16:58 -0300

    Null
    java.lang.NullPointerException:

--- exercicios/src/classe/ValorNulo.java
+++ exercicios/src/classe/ValorNulo.java
@@ -7,8 +7,8 @@ public class ValorNulo {
 		String s1 = "";
 		System.out.println(s1.concat("!!!!!!"));
 
-		String s2 = null;
-		System.out.println(s2.concat("!!!!!!!!")); //java.lang.NullPointerException:
+		//String s2 = null;
+		//System.out.println(s2.concat("!!!!!!!!")); //java.lang.NullPointerException:
 	}
 
 }
