7bb0c11632d8a5bd514faa82835471db5c39151e
joseluizs <joseluiz.smarques@gmail.com>
2023-01-09 11:22:41 -0300

    Equals

---
+++ exercicios/src/classe/Equals.java
@@ -0,0 +1,23 @@
+package classe;
+
+public class Equals {
+	
+	public static void main(String[] args) {
+		
+		Usuario u1 = new Usuario();
+		u1.nome = "Luiz";
+		u1.email = "luiz@gmail.com";
+		
+		Usuario u2 = new Usuario();
+		u2.nome = "Luiz";
+		u2.email = "luiz@gmail.com";
+		
+		System.out.println(u1 == u2);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+		
+		
+		
+	}
+
+}
