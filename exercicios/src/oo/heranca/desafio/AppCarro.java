535aed71195bcf6432d8be0cbf6425821bd169e7
joseluizs <joseluiz.smarques@gmail.com>
2023-01-11 09:18:19 -0300

    Desafio Herança

---
+++ exercicios/src/oo/heranca/desafio/AppCarro.java
@@ -0,0 +1,24 @@
+package oo.heranca.desafio;
+
+public class AppCarro {
+
+	public static void main(String[] args) {
+		Carro c = new Civic();
+		
+		c.acelerar();
+		System.out.println(c);
+		c.acelerar();
+		System.out.println(c);
+		c.acelerar();
+		System.out.println(c);
+		
+		Carro f = new Ferrari();
+		f.acelerar();
+		System.out.println(f);
+		f.acelerar();
+		System.out.println(f);
+		f.acelerar();
+		System.out.println(f);
+	}
+
+}
