329e885185ae7f21e2572424ba5a1e8353c1ec8d
joseluizs <joseluiz.smarques@gmail.com>
2023-01-12 19:59:39 -0300

    Foreach Funcional #02

--- exercicios/src/lambdas/Foreach.java
+++ exercicios/src/lambdas/Foreach.java
@@ -18,10 +18,17 @@ public class Foreach {
 		aprovados.forEach((nomes) -> {System.out.println(nomes + "!!! ");});
 		System.out.println("\nCom Lambda #01 s/ () {}; ...");
 		aprovados.forEach(nomes -> System.out.println(nomes + "!!! ")); // com um paramêtro não precisa () {}
+		System.out.println("\nCom Lambda #02 c/ função personalizada meuImprimir ...");
+		aprovados.forEach(nomes -> meuImprimir(nomes)); // com um paramêtro não precisa () {}
 
-		System.out.println("\nMethod Reference......");
+		System.out.println("\nMethod Reference #01......");
 		aprovados.forEach(System.out::println);
-		
+		System.out.println("\nMethod Reference #02 c/ função personalizada meuImprimir ...");
+		aprovados.forEach(Foreach::meuImprimir);
+	
 	}
 
+	static void meuImprimir(String nome) {
+		System.out.println("Oi! Meu nome é: " + nome);
+	}
 }
