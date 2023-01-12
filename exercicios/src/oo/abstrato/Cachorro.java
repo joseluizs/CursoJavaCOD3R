834061c0bb95b65395e413142b937f1ea50dc147
joseluizs <joseluiz.smarques@gmail.com>
2023-01-12 17:10:45 -0300

    Classe Abstrata

---
+++ exercicios/src/oo/abstrato/Cachorro.java
@@ -0,0 +1,13 @@
+package oo.abstrato;
+
+public class Cachorro extends Mamifero{
+
+	/*
+	 * @Override public String mover() { return "usando as patas"; }
+	 */
+	@Override
+	public String mamar() {
+		return "Tomando leite:";
+	}
+
+}
