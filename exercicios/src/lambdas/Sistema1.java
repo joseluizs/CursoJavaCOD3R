a251d700fc16f53f5f1df4a4332e1615cd0e029f
joseluizs <joseluiz.smarques@gmail.com>
2023-01-12 19:13:01 -0300

    Lambdas

--- exercicios/src/lambdas/Sistema1.java
+++ exercicios/src/lambdas/Sistema1.java
@@ -4,10 +4,16 @@ public class Sistema1 {
 
 	public static void main(String[] args) {
 		Calculo c = new Somar();
-		System.out.println(c.executar(2, 3));
+		System.out.println("Somar : " + c.executar(2, 3));// polimorfismo
 		
 		c = new Multiplicar();
-		System.out.println(c.executar(2, 3));// polimorfismo
+		System.out.println("Multiplicar " + c.executar(2, 3));// polimorfismo
+		
+		c =  new Dividir();
+		System.out.println("Dividir " + c.executar(2, 3));// polimorfismo
+		
+		c = new Diminuir();
+		System.out.println("Diminuir " + c.executar(2, 3));// polimorfismo
 
 	}
 }
--- exercicios/src/lambdas/Sistema2.java
+++ exercicios/src/lambdas/Sistema2.java
@@ -4,14 +4,22 @@ public class Sistema2 {
 
 	public static void main(String[] args) {
 		Calculo c = (x, y) -> {return x + y; };
-		System.out.println(c.executar(2, 3));
+		System.out.println("Somar c/ corpo da função:" + c.executar(2, 3));
 		c = (x, y) -> {return x * y; };
-		System.out.println(c.executar(2, 3));
+		System.out.println("Multiplicar c/ corpo da função: " + c.executar(2, 3));
+		c = (x, y) -> {return x - y; };
+		System.out.println("Diminuir c/ corpo da função: " + c.executar(2, 3));
+		c = (x, y) -> {return x / y; };
+		System.out.println("Dividir c/ corpo da função: " + c.executar(2, 3));
 		
 		c = (x, y) -> x + y;
-		System.out.println(c.executar(2, 3));
+		System.out.println("Somar s/ corpo da função:" + c.executar(2, 3));
 		c = (x, y) -> x * y;
-		System.out.println(c.executar(2, 3));
+		System.out.println("Multiplicar s/ corpo da função: " + c.executar(2, 3));
+		c = (x, y) -> x - y;
+		System.out.println("Diminuir s/ corpo da função: " + c.executar(2, 3));
+		c = (x, y) -> x / y;
+		System.out.println("Dividir s/ corpo da função: " + c.executar(2, 3));
 
 	}
 
