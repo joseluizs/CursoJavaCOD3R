af28ff49bc04e82a6d2aa3cb413e9c0823c5264b
joseluizs <joseluiz.smarques@gmail.com>
2023-01-13 10:06:41 -0300

    Function

---
+++ exercicios/src/lambdas/Funcao.java
@@ -0,0 +1,33 @@
+package lambdas;
+
+import java.util.function.Function;
+
+public class Funcao {
+
+	public static void main(String[] args) {
+		Function<Integer, String> parOuImpar = num -> num % 2 == 0 ? "Par" : "Impar";
+		
+		Function<String, String> oResultadoE = valor -> "O resultado e: " + valor;
+		
+		Function<String, String> empolgado = valor -> valor + "!!!";
+		Function<String, String> duvida = valor -> valor + "???";
+		
+		System.out.println(parOuImpar.apply(32));
+		String resultadoFinal = parOuImpar
+				.andThen(oResultadoE)
+				.andThen(empolgado)
+				.apply(32);
+		
+		System.out.println(resultadoFinal);
+		
+		System.out.println(parOuImpar.apply(33));
+		String resultadoFinal1 = parOuImpar
+				.andThen(oResultadoE)
+				.andThen(duvida)
+				.apply(33);
+
+		System.out.println(resultadoFinal1);
+
+	}
+
+}
