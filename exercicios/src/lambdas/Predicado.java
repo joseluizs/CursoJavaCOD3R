55964631a0b196e7c345bfe24fcd3f5307ada306
joseluizs <joseluiz.smarques@gmail.com>
2023-01-13 09:25:02 -0300

    Predicate #01

---
+++ exercicios/src/lambdas/Predicado.java
@@ -0,0 +1,14 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+	
+	public static void main(String[] args) {
+		Predicate<Produto> isCaro = p -> (p.preco * (1 - p.desconto)) >= 750;
+		
+		Produto p = new Produto("Notebook", 3893.89, 0.15);
+		System.out.println(isCaro.test(p));
+	}
+
+}
