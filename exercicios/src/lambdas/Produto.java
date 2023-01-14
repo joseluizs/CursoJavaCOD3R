60a536f44ccedb44fec6966db41adb68a8f666dd
joseluizs <joseluiz.smarques@gmail.com>
2023-01-14 14:57:36 -0300

    Desafio Binary Operator

--- exercicios/src/lambdas/Produto.java
+++ exercicios/src/lambdas/Produto.java
@@ -3,10 +3,10 @@ package lambdas;
 public class Produto {
 
 	final String nome;
-	final double preco;
-	final double desconto;
+	final Double preco;
+	final Double desconto;
 	
-	public Produto(String nome, double preco, double desconto) {
+	public Produto(String nome, Double preco, Double desconto) {
 		this.nome = nome;
 		this.preco = preco;
 		this.desconto = desconto;
