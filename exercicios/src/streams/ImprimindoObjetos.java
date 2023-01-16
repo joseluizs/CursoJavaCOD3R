5b851a3e0f89bc3722fdca9e71820c4206612da8
joseluizs <joseluiz.smarques@gmail.com>
2023-01-15 21:03:03 -0300

    List/Iterator/Stream

--- exercicios/src/streams/ImprimindoObjetos.java
+++ exercicios/src/streams/ImprimindoObjetos.java
@@ -1,9 +1,29 @@
 package streams;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class ImprimindoObjetos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> aprovados = Arrays.asList("Luiz", "Cris", "Esther");
+		
+		System.out.println("Usando o foreach!");
+		for (String nomes : aprovados) {
+			System.out.println(nomes);
+		}
+		
+		System.out.println("\nUsando Iterator!  ");
+		Iterator<String> it = aprovados.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());			
+		}
+		
+		System.out.println("\nUsando Stream! ");
+		Stream<String> stream = aprovados.stream();
+		stream.forEach(System.out::println);
 
 	}
 
