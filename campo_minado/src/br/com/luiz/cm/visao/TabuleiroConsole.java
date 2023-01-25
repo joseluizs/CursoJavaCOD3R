906496b530a86951ed7651b12eb83b669c25abb4
joseluizs <joseluiz.smarques@gmail.com>
2023-01-25 20:20:32 -0300

    Exibir Todos os Campos

--- campo_minado/src/br/com/luiz/cm/visao/TabuleiroConsole.java
+++ campo_minado/src/br/com/luiz/cm/visao/TabuleiroConsole.java
@@ -55,10 +55,10 @@ public class TabuleiroConsole {
 				} else if ("2".equals(digitado)) {
 					tabuleiro.alternarMarcacao(xy.next(), xy.next());
 				}
-				
 			}
 			System.out.println("Você ganhou!");
 		} catch (ExplosaoException e) {
+			System.out.println(tabuleiro);
 			System.out.println("Você perdeu!");
 		}	
 	}
