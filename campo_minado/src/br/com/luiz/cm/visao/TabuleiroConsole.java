c9ee4e27d52824d461ca0a64cf345672dae7d36c
joseluizs <joseluiz.smarques@gmail.com>
2023-01-25 20:26:49 -0300

    Correção na Distribuição das Minas

--- campo_minado/src/br/com/luiz/cm/visao/TabuleiroConsole.java
+++ campo_minado/src/br/com/luiz/cm/visao/TabuleiroConsole.java
@@ -56,6 +56,7 @@ public class TabuleiroConsole {
 					tabuleiro.alternarMarcacao(xy.next(), xy.next());
 				}
 			}
+			System.out.println(tabuleiro);
 			System.out.println("Você ganhou!");
 		} catch (ExplosaoException e) {
 			System.out.println(tabuleiro);
