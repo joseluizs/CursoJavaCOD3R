17f410b99f260c26a3a5649136622da9260f2e97
joseluizs <joseluiz.smarques@gmail.com>
2023-01-25 20:12:10 -0300

    Classe Tabuleiro Console

--- campo_minado/src/br/com/luiz/cm/aplicacao/Aplicacao.java
+++ campo_minado/src/br/com/luiz/cm/aplicacao/Aplicacao.java
@@ -1,18 +1,14 @@
 package br.com.luiz.cm.aplicacao;
 
 import br.com.luiz.cm.modelo.Tabuleiro;
+import br.com.luiz.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 
 		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-		tabuleiro.abrir(3, 3);
-		tabuleiro.alternarMarcacao(4, 4);
-		tabuleiro.alternarMarcacao(4, 5);
-		
-		System.out.println(tabuleiro);
-
+		new TabuleiroConsole(tabuleiro);
 	}
 
 }
