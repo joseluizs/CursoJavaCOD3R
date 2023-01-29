1fc7e12c45de3659766e3e7f15be807f836aed1e
joseluizs <joseluiz.smarques@gmail.com>
2023-01-29 20:33:00 -0300

    Generics e Herança #02

---
+++ exercicios/src/generics/CaixaNumeroTeste.java
@@ -0,0 +1,16 @@
+package generics;
+
+public class CaixaNumeroTeste {
+
+	public static void main(String[] args) {
+		CaixaNumero<Double> caixa1 = new CaixaNumero<>();
+		caixa1.guardar(2.3);
+		System.out.println(caixa1.abrir());
+		
+		CaixaNumero<Integer> caixa2 = new CaixaNumero<>();
+		caixa2.guardar(123);
+		System.out.println(caixa2.abrir());
+
+	}
+
+}
