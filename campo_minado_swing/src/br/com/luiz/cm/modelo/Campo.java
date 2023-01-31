390152a612ef486dfc5d38735bb00062b9643a99
joseluizs <joseluiz.smarques@gmail.com>
2023-01-31 18:18:23 -0300

    Interface Gráfica do Campo Minado #04

--- campo_minado_swing/src/br/com/luiz/cm/modelo/Campo.java
+++ campo_minado_swing/src/br/com/luiz/cm/modelo/Campo.java
@@ -48,7 +48,7 @@ public class Campo {
 		}
 	}
 	
-	void alternarMarcacao() {
+	public void alternarMarcacao() {
 		if (!aberto) {
 			marcado = !marcado;
 			if (marcado) {
@@ -59,7 +59,7 @@ public class Campo {
 		}
 	}
 	
-	boolean abrir() {
+	public boolean abrir() {
 		if (!aberto && !marcado) {
 			if (minado) {
 				notificarObservadores(CampoEvento.EXPLODIR);
@@ -76,7 +76,7 @@ public class Campo {
 		}
 	}
 	
-	boolean vizinhacaSegura() {
+	public boolean vizinhacaSegura() {
 		return vizinhos.stream().noneMatch(v -> v.minado);
 	}
 	
@@ -124,8 +124,8 @@ public class Campo {
 		return desvendado || protegido;
 	}
 	
-	long minasNaVizinhanca() {
-		return vizinhos.stream().filter(v -> v.minado).count();
+	public int minasNaVizinhanca() {
+		return (int)vizinhos.stream().filter(v -> v.minado).count();
 	}
 	
 	void reiniciar() {
--- campo_minado_swing/src/br/com/luiz/cm/visao/BotaoCampo.java
+++ campo_minado_swing/src/br/com/luiz/cm/visao/BotaoCampo.java
@@ -1,6 +1,8 @@
 package br.com.luiz.cm.visao;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -10,7 +12,7 @@ import br.com.luiz.cm.modelo.CampoEvento;
 import br.com.luiz.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton implements CampoObservador{
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener{
 
 	private final Color BG_PADRAO = new Color(184, 184, 184);
 	private final Color BG_MARCAR = new Color(8, 179, 247);
@@ -23,6 +25,9 @@ public class BotaoCampo extends JButton implements CampoObservador{
 		this.campo = campo;
 		setBorder(BorderFactory.createBevelBorder(0));
 		setBackground(BG_PADRAO);
+		
+		addMouseListener(this);
+		campo.registrarObservador(this);
 	}
 
 	@Override
@@ -57,6 +62,47 @@ public class BotaoCampo extends JButton implements CampoObservador{
 	}
 
 	private void aplicarEstiloAbrir() {
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
 		
+		switch (campo.minasNaVizinhanca()) {
+		case 1:
+			setForeground(TEXTO_VERDE);
+			break;
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+		case 3:
+			setForeground(Color.YELLOW);
+			break;
+		case 4:
+		case 5:
+		case 6:
+			setForeground(Color.RED);
+			break;
+		default:
+			setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhacaSegura() ? campo.minasNaVizinhanca() + "" : "";
+		setText(valor);
 	}
+	
+	//Interface dos enventos do Mouse
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == 1) {
+			campo.abrir();
+		} else {
+			campo.alternarMarcacao();
+		}
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {}
+	@Override
+	public void mouseEntered(MouseEvent e) {}
+	@Override
+	public void mouseExited(MouseEvent e) {}
+	@Override
+	public void mouseReleased(MouseEvent e) {}
 }
