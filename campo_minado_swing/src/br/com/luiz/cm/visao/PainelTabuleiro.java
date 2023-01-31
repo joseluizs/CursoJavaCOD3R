d77db7be8afb96c01086841b3806e1d78c966e14
joseluizs <joseluiz.smarques@gmail.com>
2023-01-31 11:05:59 -0300

    Interface Gráfica do Campo Minado #01

---
+++ campo_minado_swing/src/br/com/luiz/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.luiz.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.luiz.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+	}
+}
