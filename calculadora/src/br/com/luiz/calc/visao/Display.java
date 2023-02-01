611162ff00231360f893274ed51a01c8bd873f65
joseluizs <joseluiz.smarques@gmail.com>
2023-02-01 09:42:57 -0300

    Componente Teclado #01

---
+++ calculadora/src/br/com/luiz/calc/visao/Display.java
@@ -0,0 +1,25 @@
+package br.com.luiz.calc.visao;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Display extends JPanel{
+	
+	private final JLabel label;
+	
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("123,04");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		add(label);
+		
+	}
+
+}
