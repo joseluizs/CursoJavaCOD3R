5ced1544b1b0dc44f54243c585b35151d18f5fbb
joseluizs <joseluiz.smarques@gmail.com>
2023-02-01 09:49:27 -0300

    Componente Teclado #02

--- calculadora/src/br/com/luiz/calc/visao/Teclado.java
+++ calculadora/src/br/com/luiz/calc/visao/Teclado.java
@@ -3,7 +3,6 @@ package br.com.luiz.calc.visao;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
@@ -20,10 +19,14 @@ public class Teclado extends JPanel{
 		GridBagConstraints c = new GridBagConstraints();
 		
 		setLayout(layout);
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		
 		//Linha 1
+		c.gridwidth = 3;
 		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
-		adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
-		adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+		c.gridwidth = 1;
 		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 		
 		//Linha 2
@@ -45,8 +48,9 @@ public class Teclado extends JPanel{
 		adicionarBotao("+", COR_LARANJA, c, 3, 3);
 		
 		////Linha 5
+		c.gridwidth = 2;
 		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
-		adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+		c.gridwidth = 1;
 		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
 		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
