9c216b5591d67671be7a4a61d9fa17bcb2486f5a
joseluizs <joseluiz.smarques@gmail.com>
2023-02-10 15:38:26 -0300

    Exports To

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -2,4 +2,7 @@ module app.calculo {
 	requires app.logging;
 	exports br.com.luiz.app.calculo;
 	
+	exports br.com.luiz.app.calculo.interno
+	to app.financeiro;
+	
 }
--- app-financeiro/src/br/com/luiz/app/financeiro/Teste.java
+++ app-financeiro/src/br/com/luiz/app/financeiro/Teste.java
@@ -1,12 +1,16 @@
 package br.com.luiz.app.financeiro;
 
 import br.com.luiz.app.calculo.Calculadora;
+import br.com.luiz.app.calculo.interno.OperacoesAritmeticas;
 
 public class Teste {
 
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println(calc.soma(2, 3, 4));
+		
+		OperacoesAritmeticas op = new OperacoesAritmeticas();
+		System.out.println(op.soma(4, 5, 6));
 	}
 
 }
