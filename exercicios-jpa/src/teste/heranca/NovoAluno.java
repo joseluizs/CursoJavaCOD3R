232eae227343a179f7126786b248d22885ab96ca
joseluizs <joseluiz.smarques@gmail.com>
2023-02-10 12:13:10 -0300

    Herança #01

---
+++ exercicios-jpa/src/teste/heranca/NovoAluno.java
@@ -0,0 +1,20 @@
+package teste.heranca;
+
+import infra.DAO;
+import modelo.heranca.Aluno;
+import modelo.heranca.AlunoBolsista;
+
+public class NovoAluno {
+
+	public static void main(String[] args) {
+		DAO<Aluno> dao = new DAO<>();
+		
+		Aluno aluno =  new Aluno(123L, "Luiz");
+		AlunoBolsista alunoBolsista = new AlunoBolsista(345L, "Luizinho", 1000);
+		
+		dao.incluirAtomico(aluno);
+		dao.incluirAtomico(alunoBolsista);
+
+	}
+
+}
