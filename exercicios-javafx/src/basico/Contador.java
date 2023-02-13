ca0a564ce621e990fb831cddb44afa5a5ff01aba
joseluizs <joseluiz.smarques@gmail.com>
2023-02-13 17:26:45 -0300

    Contador #05

--- exercicios-javafx/src/basico/Contador.java
+++ exercicios-javafx/src/basico/Contador.java
@@ -13,6 +13,20 @@ public class Contador extends Application {
 
 	private int contador = 0;
 
+	private void atualizarLabelNumero(Label label) {
+		label.setText(Integer.toString(contador));
+
+		label.getStyleClass().remove("verde");
+		label.getStyleClass().remove("vermelho");
+
+		if (contador > 0) {
+			label.getStyleClass().add("verde");
+		} else if (contador < 0) {
+
+			label.getStyleClass().add("vermelho");
+		}
+	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
@@ -25,14 +39,14 @@ public class Contador extends Application {
 		Button botaoDecremento = new Button("-");
 		botaoDecremento.getStyleClass().add("botoes");
 		botaoDecremento.setOnAction(e -> {
-			labelNumero.setText(Integer.toString(contador));
 			contador--;
+			atualizarLabelNumero(labelNumero);
 		});
 		Button botaoIncremento = new Button("+");
 		botaoIncremento.getStyleClass().add("botoes");
 		botaoIncremento.setOnAction(e -> {
 			contador++;
-			labelNumero.setText(Integer.toString(contador));
+			atualizarLabelNumero(labelNumero);
 		});
 
 		HBox boxBotoes = new HBox();
