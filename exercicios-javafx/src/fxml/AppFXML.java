98ac6a86a3fe67020cbae15904e63a8596ce040b
joseluizs <joseluiz.smarques@gmail.com>
2023-02-16 13:30:31 -0300

    Alteração

--- exercicios-javafx/src/fxml/AppFXML.java
+++ exercicios-javafx/src/fxml/AppFXML.java
@@ -16,7 +16,7 @@ public class AppFXML extends Application {
 		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
 		GridPane raiz = FXMLLoader.load(arquivoFXML);
 		
-		Scene cena = new Scene(raiz, 400, 400);
+		Scene cena = new Scene(raiz, 350, 350);
 		cena.getStylesheets().add(arquivoCSS);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Tela de Login");
