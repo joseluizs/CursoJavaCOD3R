9b873bb6c36aad013b08a2b40e4bd48092b9db77
joseluizs <joseluiz.smarques@gmail.com>
2023-02-16 14:45:32 -0300

    ControlsFX

--- exercicios-javafx/src/fxml/LoginControlador.java
+++ exercicios-javafx/src/fxml/LoginControlador.java
@@ -1,6 +1,9 @@
 package fxml;
 
+import org.controlsfx.control.Notifications;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -12,6 +15,15 @@ public class LoginControlador {
 	private PasswordField campoSenha;
 	
 	public void entrar() {
+		boolean emailValido = campoEmail.getText().equals("luiz@gmail.com");
+		boolean senhaValida = campoSenha.getText().equals("123456");
+		
+		if (emailValido && senhaValida) {
+			Notifications.create().position(Pos.TOP_CENTER).title("Login FXML").text("Login efetuado com sucesso!").showInformation();
+			
+		} else {
+			Notifications.create().position(Pos.TOP_CENTER).title("Login FXML").text("Erro ao efetuar o login!").showError();
+		}
 		System.out.println(campoEmail.getText());
 		System.out.println(campoSenha.getText());
 	}
