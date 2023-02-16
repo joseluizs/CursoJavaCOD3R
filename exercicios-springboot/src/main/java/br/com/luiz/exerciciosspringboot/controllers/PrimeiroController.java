2c7c7c53f9b1261823d99197384f98443ee20f32
joseluizs <joseluiz.smarques@gmail.com>
2023-02-16 16:40:18 -0300

    Primeiro Web Service

---
+++ exercicios-springboot/src/main/java/br/com/luiz/exerciciosspringboot/controllers/PrimeiroController.java
@@ -0,0 +1,14 @@
+package br.com.luiz.exerciciosspringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+	//@RequestMapping(method = RequestMethod.GET, path = "/ola")
+	@GetMapping(path = "/ola")
+	public String ola() {
+		return "Olá Spring Boot!";
+	}
+}
